b5ccae35c7eac8a87b3865cee1678c79e71b809b
Nicolas H <hubnersantos@aol.com>
2024-08-06 22:03:42 -0300

    [feat] Adapt new design

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,28 +1,37 @@
-import { Box, Button, Typography } from "@mui/material";
+import {Box, Button, Icon, Typography} from "@mui/material";
 import styles from './style.module.css';
-import { projects } from './projects';
-
-interface IProject {
-  name: string;
-  Language: string;
-  image: string;
-  link: string;
-}
+import AndroidIcon from '@mui/icons-material/Android';
+import IOSIcon from '@mui/icons-material/Apple';
+import {projects, ProjectType} from './projects';
 
 export default function Project() {
   return (
     <>
       <Typography mt={20} id="Projects" variant="h3" py={5}>Projects</Typography>
-      {projects.map((project: IProject, i: number) => (
-        <Box key={i} sx={{ display: 'flex', flexDirection: 'row', marginY: 10, justifyContent: 'center'}}>
+        <Box className={styles.ContainerProjects}>
+      {projects.map((project: ProjectType, i: number) => (
+        <Box key={i} className={styles.ContainerViewProject}>
           <img src={project.image} alt={project.name} className={styles.image} />
           <Box sx={{ display: 'flex', flexDirection: 'column',width: '400px', justifyContent: 'space-around', alignItems: 'center'}}>
             <Typography variant="h5">{project.name}</Typography>
+
             <p><strong>Tecnologias Utilizadas</strong>{`: ${project.Language}`}</p>
-            <Button sx={{backgroundColor: 'white', width: '60%'}} href={project.link}>Link to{` ${project.name}`}</Button>
+
+              <Icon className={styles.Icon}
+ onClick={() => window.open(project.linkAndroid, '_blank')}
+              >
+                  <AndroidIcon sx={{color: 'black', width: '20px', height: '20px'}} />
+              </Icon>
+
+              <Icon className={styles.Icon}
+ onClick={() => window.open(project.linkiOS, '_blank')}
+              >
+                  <IOSIcon sx={{color: 'black', width: '20px', height: '20px'}} />
+              </Icon>
           </Box>
         </Box>
       ))}
+            </Box>
     </>
   )
 }
--- components/Projects/projects.ts
+++ components/Projects/projects.ts
@@ -1,26 +1,21 @@
-export const projects =[
+export type ProjectType = {
+  name: string;
+  description: string;
+  Language: string;
+  image: string;
+  link?: string;
+  linkAndroid?: string;
+  linkiOS?: string;
+}
+
+export const projects : ProjectType[] =[
   {
-    name: 'App de Receitas',
-    Language: 'Next.js, TypeScript, MaterialUI',
+    name: 'Pontua',
+    description: 'A simple Pontua app to help you keep track of your work progress.',
+    Language: 'React Native, Unistyles, Unit Test, NestJs, TypeScript',
     image: '/apprecipes.png',
-    link: 'https://app-recipes-next-delnxqvy4-nicolashubner.vercel.app/'
+    link: 'https://github.com/pontua/pontua',
+    linkiOS: 'https://apps.apple.com/br/app/pontua/id6503622178',
+    linkAndroid: 'https://play.google.com/store/apps/details?id=com.pontua.pontuapp&hl=en_US&pli=1'
   },
-  {
-    name: 'Shopping Cart',
-    Language: 'TypeScript, React, Styled Components, Redux',
-    image: '/ShoppingCart.png',
-    link: 'https://shoppingcarthubner.herokuapp.com/'
-  },
-  {
-    name: 'Pokedex',
-    Language: 'JavaScrip, HTML, CSS',
-    image: '/Pokedex.png',
-    link: 'https://marcosridley.github.io/private-pokedex/'
-  },
-  {
-    name: 'WeatherApp',
-    Language: 'JavaScript, HTML, CSS',
-    image: 'WeatherApp.png',
-    link: 'https://nicolashubner.github.io/WeatherApp/'
-  }
 ]
